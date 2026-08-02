8fa4519f4ff2e101839956da1dc8992a4a250553
KZNowhere <supavich.032547@gmail.com>
2026-08-02 20:00:30 +0700

    Fix Compose RowScope weight compilation

--- android-app/app/src/main/java/com/nowtuneup/app/MainActivity.kt
+++ android-app/app/src/main/java/com/nowtuneup/app/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
