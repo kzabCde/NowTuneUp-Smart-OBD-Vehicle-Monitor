0e70d5b35596926ca4f5429f58d6ab1caadf4144
KZNowhere <supavich.032547@gmail.com>
2026-08-03 17:36:49 +0700

    Bump NowTuneUp to version 1.6.2 build 13

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.nowtuneup.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.6.1"
+        versionCode = 13
+        versionName = "1.6.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("boolean", "MOCK_OBD_DEFAULT", "false")
@@ -34,8 +34,8 @@ android {
     buildTypes {
         debug {
             applicationIdSuffix = ".debug"
-            versionNameSuffix = "-bluetooth-test"
-            manifestPlaceholders["appLabel"] = "NowTuneUp Bluetooth Test"
+            versionNameSuffix = "-ui-test"
+            manifestPlaceholders["appLabel"] = "NowTuneUp 1.6.2 Test"
             buildConfigField("boolean", "MOCK_OBD_DEFAULT", "false")
         }
         release {
@@ -61,9 +61,9 @@ android {
         outputs.all {
             val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
             output.outputFileName = if (buildType.name == "release") {
-                "NowTuneUp-v1.6.1-release.apk"
+                "NowTuneUp-v1.6.2-release.apk"
             } else {
-                "NowTuneUp-v1.6.1-bluetooth-test.apk"
+                "NowTuneUp-v1.6.2-ui-test.apk"
             }
         }
     }
